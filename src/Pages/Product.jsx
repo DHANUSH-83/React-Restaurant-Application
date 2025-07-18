1b94b0286f0a7e37fe8a17d720fc442fae68b7a2
DHANUSH-83 <dhanushec8300@gmail.com>
2025-07-18 15:09:09 +0530

    updates

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import { CartContext } from '../Context/CartProvider';
 import './Styles/product.scss';
 import MyNavbar from '../Components/Navbar/MyNavbar';
-import { Nav, Container, Row, Col, Card, Modal,Form } from 'react-bootstrap';
+import { Nav, Container, Row, Col, Card, Modal, Form } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom';
 import pg from '../assets/pg.jpeg'
 
